60c243fa89c52af31e4cb7d319956da64a593bb2
Akkissss23 <2244124@sliet.ac.in>
2025-01-31 03:07:38 +0530

    Add files via upload

---
+++ movie recommendation/app.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import requests
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?page=1&api_key=36e8de2196c54bb0e16219402965efea'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movie_posters = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # fetch poster from API
+        recommended_movie_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movie_posters
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('BETTER VERSE')
+st.header("Movie Recommended System")
+st.markdown("--By Arpit")
+# st.latex(r""" e^{i\pi} + 1 = 0 """)
+# st.text("Find the recommendations for your watchlist")
+# st.subheader("My header")
+# st.subheader("My sub")
+# st.code("for i in range(8): foo()")
+# st.image("/Users/arpit_lal/Downloads/—Pngtree—movie\ film\ roll\ black\ and_8529744.png")
+# # st.audio(data)
+# # st.video(data)
+# # st.video(data, subtitles="./subs.vtt")
+# st.logo("Users/arpit_lal/Downloads/minolta-1320027_1280.jpg")
+
+selected_movie_name = st.selectbox('Search your movie', movies['title'].values)
+
+if st.button('recommend'):
+    names,posters = recommend(selected_movie_name)
+
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col5:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
